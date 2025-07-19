f5213f245038bad6f46dfa68d7d9e418d237aae7
xam2008 <104933360+xam2008@users.noreply.github.com>
2025-07-20 01:31:29 +0200

    Ajusted root dir for Vercel deployment

--- src/Containers/app/Developer/Network/create_dev.php
+++ src/Containers/app/Developer/Network/create_dev.php
@@ -7,3 +7,9 @@ $USER = $env['USER'];
 $PASS = $env['PASS'];
 
 $con = new mysqli($host, $USER, $PASS, $DB);
+
+$teamName = $_POST['teamname'];
+$users = "";
+$query = "INSERT INTO `Dev_teams`(`Teamname`, `licenseKey`, `isActive`) VALUES ('$teamName','NULL','1')";
+
+$result = mysqli_query($con, $query);
